0a3623841e46ce4012de12e7a18b9c85f440b75b
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2023-12-27 18:05:26 -0300

    chore: shadcn ui adjusts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,24 @@ import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "~/components/lib/utils";
 import "~/styles/globals.css";
 
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <main className={cn("font-sans",fontSans.variable)}>
+        <Component {...pageProps} />
+        </main>
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
