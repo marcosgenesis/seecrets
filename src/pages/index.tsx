179ef8875484b8fb7ed8feae3ff825485e03d915
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2024-01-04 16:41:43 -0300

    feat: form component to only one visualization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,31 @@
 import { useUser } from "@clerk/nextjs";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { MessageCircle, RefreshCcw, ThumbsDown, ThumbsUp } from "lucide-react";
+import {
+  EyeIcon,
+  MessageCircle,
+  RefreshCcw,
+  ThumbsDown,
+  ThumbsUp,
+  ViewIcon,
+} from "lucide-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import MyPosts from "~/components/MyPosts";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/button";
+import { Card, CardContent } from "~/components/ui/card";
 import {
-  Card,
-  CardContent,
-} from "~/components/ui/card";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "~/components/ui/form";
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
 import { Input } from "~/components/ui/input";
 import { Textarea } from "~/components/ui/textarea";
+import { Toggle } from "~/components/ui/toggle";
 import { ToggleGroup, ToggleGroupItem } from "~/components/ui/toggle-group";
 import { api } from "~/utils/api";
 
@@ -28,7 +41,7 @@ const newPostSchema = z.object({
 export default function Home() {
   const { user } = useUser();
   const createPost = api.post.create.useMutation();
-  const getPosts = api.post.getAllFromUser.useQuery({ userId: user?.id ?? '' });
+  const getPosts = api.post.getAllFromUser.useQuery({ userId: user?.id ?? "" });
   const form = useForm<z.infer<typeof newPostSchema>>({
     resolver: zodResolver(newPostSchema),
     defaultValues: {
@@ -49,29 +62,39 @@ export default function Home() {
   }
 
   return (
-    <div >
-      <div className="flex h-dvh items-center justify-center flex-col">
-        <div className="h-1/2 flex flex-col justify-evenly items-center">
-          <p>Esta é uma mensagem aleatória de um usuário qualquer, pode ser de qualquer pessoa do mundo</p>
-          <div className="flex justify-between w-1/2">
-            <Button variant={'outline'}><RefreshCcw size={16} className="mr-2" /> Atualizar</Button>
+    <div>
+      <div className="flex h-dvh flex-col items-center justify-center">
+        <div className="flex h-1/2 flex-col items-center justify-evenly">
+          <p>
+            Esta é uma mensagem aleatória de um usuário qualquer, pode ser de
+            qualquer pessoa do mundo
+          </p>
+          <div className="flex w-1/2 justify-between">
+            <Button variant={"outline"}>
+              <RefreshCcw size={16} className="mr-2" /> Atualizar
+            </Button>
             <div className="flex gap-2">
               <ToggleGroup type="single">
-                <ToggleGroupItem value="like" variant={'outline'}><ThumbsUp size={16} /></ToggleGroupItem>
-                <ToggleGroupItem value="deslike" variant={'outline'}><ThumbsDown size={16} /></ToggleGroupItem>
+                <ToggleGroupItem value="like" variant={"outline"}>
+                  <ThumbsUp size={16} />
+                </ToggleGroupItem>
+                <ToggleGroupItem value="deslike" variant={"outline"}>
+                  <ThumbsDown size={16} />
+                </ToggleGroupItem>
               </ToggleGroup>
-              <Button variant={'outline'}><MessageCircle size={16} /></Button>
-
+              <Button variant={"outline"}>
+                <MessageCircle size={16} />
+              </Button>
             </div>
           </div>
         </div>
-        <div className="my-4 flex gap-4 items-start">
+        <div className="my-4 flex items-start gap-4">
           <Card>
             <CardContent className="flex flex-col gap-2 py-4">
               <div className="flex items-center justify-center gap-2">
                 <Avatar>
                   <AvatarImage src={user?.imageUrl} />
-                  <AvatarFallback>{user?.firstName[0] ?? 'U'}</AvatarFallback>
+                  <AvatarFallback>{user?.firstName[0] ?? "U"}</AvatarFallback>
                 </Avatar>
                 <p>Bem vindo novamente, {user?.firstName}!</p>
               </div>
@@ -87,7 +110,7 @@ export default function Home() {
                       <FormItem>
                         <FormLabel>Título</FormLabel>
                         <FormControl>
-                          <Input placeholder="shadcn" {...field} />
+                          <Input placeholder="Título da postagem" {...field} />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
@@ -100,13 +123,34 @@ export default function Home() {
                       <FormItem>
                         <FormLabel>Descrição</FormLabel>
                         <FormControl>
-                          <Textarea placeholder="shadcn" {...field} />
+                          <Textarea
+                            placeholder="Descrição da postagem"
+                            {...field}
+                          />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
                     )}
                   />
-                  <Button className="w-full" type="submit" disabled={createPost.isLoading}>{createPost.isLoading ? 'Carregando...' : 'Criar postagem'}</Button>
+                  <FormField
+                    control={form.control}
+                    name="content"
+                    render={({ field }) => (
+                      <div className="flex items-center gap-2">
+                        <Toggle {...field} variant={"outline"}>
+                          <ViewIcon />
+                        </Toggle>
+                        <p>Visualização única</p>
+                      </div>
+                    )}
+                  />
+                  <Button
+                    className="w-full"
+                    type="submit"
+                    disabled={createPost.isLoading}
+                  >
+                    {createPost.isLoading ? "Carregando..." : "Criar postagem"}
+                  </Button>
                 </form>
               </Form>
             </CardContent>
@@ -114,7 +158,6 @@ export default function Home() {
           <MyPosts />
         </div>
       </div>
-
     </div>
   );
 }
